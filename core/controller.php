dbb1177681f9c54708dea01561d03092add7940f
eversonsa <eversonsa@email.com>
2017-05-06 05:11:20 -0300

    Atualizalçao do constructor na classe controller
    Atualizalçao do constructor na classe controller do pacote Core

--- core/controller.php
+++ core/controller.php
@@ -2,6 +2,14 @@
 
 class controller {
     
+    protected $db;
+    
+    public functions __construct(){
+        
+        global $config;
+		$this->db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
+    }
+    
     public function loadView($viewName, $viewData = array()){
         extract($viewData);
         include 'view/'.$viewName.'.php';
